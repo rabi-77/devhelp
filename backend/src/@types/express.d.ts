96312546822ec8a853941dd77e4e67702b5074b8
rabi-77 <mohammedrabeeh63@gmail.com>
2026-02-03 18:06:34 +0530

    Week One: changes

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,13 +1,1 @@
-import { Request } from "express";
-import { TokenPayload } from "../core/application/services/ITokenService";
 
-declare global {
-  namespace Express {
-    interface Request {
-      subdomain?: string;
-      user?: TokenPayload;
-    }
-  }
-}
-
-export { };
